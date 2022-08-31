cccebaffea0b40f8b144a6cae76ba8d62f066369
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2022-08-31 21:39:04 +0100

    BST-34429 Setup tests

--- test/controllers/ApplicationSpec.scala
+++ test/controllers/ApplicationSpec.scala
@@ -1,21 +1,31 @@
 /*
  * Copyright 2022 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package uk.gov.hmrc.voafor.controllers
+package controllers
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
 import play.api.http.Status
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.api.inject.guice.GuiceApplicationBuilder
-import uk.gov.hmrc.voatctr.controllers.ApplicationController
 
-class ApplicationControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+class ApplicationSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
   override def fakeApplication(): Application =
     new GuiceApplicationBuilder()
       .configure(
@@ -26,7 +36,7 @@ class ApplicationControllerSpec extends AnyWordSpec with Matchers with GuiceOneA
 
   private val fakeRequest = FakeRequest("GET", "/")
 
-  private val controller = app.injector.instanceOf[ApplicationController]
+  private val controller = app.injector.instanceOf[controllers.Application]
 
   "GET /" should {
     "return 200" in {
