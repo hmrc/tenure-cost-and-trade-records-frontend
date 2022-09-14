daf4fc475fe5df59b29359679e498bc144bf3e4d
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2022-09-14 10:52:38 +0100

    BST-36708 Add validation error message handling (#27)

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -20,10 +20,36 @@ import play.api.Logging
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import connectors.BackendConnector
+import play.api.data.Form
+import play.api.data.Forms._
 import views.html.{login, testSign}
+import org.joda.time.DateTime
+import play.api.data.JodaForms._
+import form.{Errors, MappingSupport}
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
+
+case class LoginDetails(referenceNumber: String, postcode: String, startTime: DateTime)
+
+object LoginController {
+  val loginForm = Form(
+    mapping(
+      //format of reference number should be 7 or 8 digits then / then 3 digits
+      "referenceNumber" -> text.verifying(Errors.invalidRefNum, x => {
+        val cleanRefNumber = x.replaceAll("\\D+", "")
+        val validLength = cleanRefNumber.length > 9 && cleanRefNumber.length < 12: Boolean
+        validLength
+      }),
+      "postcode" -> text.verifying(Errors.invalidPostcodeOnLetter, pc => {
+        var cleanPostcode = pc.replaceAll("[^\\w\\d]", "")
+        cleanPostcode = cleanPostcode.patch(cleanPostcode.length - 3, " ", 0).toUpperCase
+        val isValid = cleanPostcode.matches(MappingSupport.postcodeRegex): Boolean
+        isValid
+      }),
+      "start-time" -> jodaDate("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    )(LoginDetails.apply)(LoginDetails.unapply))
+}
 
 @Singleton
 class LoginController  @Inject()(
@@ -33,14 +59,20 @@ class LoginController  @Inject()(
     connector: BackendConnector
 ) extends FrontendController(mcc) with Logging {
 
+  import LoginController.loginForm
+
   def show: Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok(login()))
+    Future.successful(Ok(login(loginForm)))
   }
 
-  def submit(referenceNumber: String, postcode: String): Action[AnyContent] = Action.async { implicit request =>
-    logger.debug(s"Signing in with: reference number : ${referenceNumber}, postcode: ${postcode}")
-    val msg = connector.testConnection(referenceNumber, postcode)
-    Future.successful(Ok(test(msg)))
+  def submit = Action.async { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => Future.successful(BadRequest(login(formWithErrors))),
+      loginData => {
+        logger.debug(s"Signing in with: reference number : ${loginData.referenceNumber}, postcode: ${loginData.postcode}")
+        Future.successful(Ok(test(connector.testConnection(loginData.referenceNumber, loginData.postcode))))
+      }
+    )
   }
 
 }
