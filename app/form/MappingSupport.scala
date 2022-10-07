dd34405b0ea155588cb02d23bb367de56efc5095
peterdrobinson <77154273+peterdrobinson@users.noreply.github.com>
2022-10-07 09:19:16 +0100

    BST-37757 - Added form to Method To Fix Current Rent to page

--- app/form/MappingSupport.scala
+++ app/form/MappingSupport.scala
@@ -48,6 +48,7 @@ object MappingSupport {
   val rentIncludeFixturesAndFittingsType: Mapping[RentIncludeFixturesAndFittings] = Forms.of[RentIncludeFixturesAndFittings]
   val rentOpenMarketValuesType: Mapping[RentOpenMarketValues] = Forms.of[RentOpenMarketValues]
   val areYouStillConnectedNoType: Mapping[AreYouStillConnectedNo] = Forms.of[AreYouStillConnectedNo]
+  val methodToFixCurrentRentsType: Mapping[MethodToFixCurrentRents] = Forms.of[MethodToFixCurrentRents]
   val postcode: Mapping[String] = PostcodeMapping.postcode()
 
   val contactDetailsMapping: Mapping[ContactDetails] =
