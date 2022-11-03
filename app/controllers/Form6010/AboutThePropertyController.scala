4a4ece5ae5b56a24a87963540b43a12d99a736c3
peterdrobinson <77154273+peterdrobinson@users.noreply.github.com>
2022-11-03 17:53:50 +0000

    BST-40014 - Amended journey for about you and about the property

--- app/controllers/Form6010/AboutThePropertyController.scala
+++ app/controllers/Form6010/AboutThePropertyController.scala
@@ -16,13 +16,11 @@
 
 package controllers.Form6010
 
-import controllers.LoginController.loginForm
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.Form6010.{aboutTheProperty, websiteForProperty}
 import form.AboutThePropertyForm.aboutThePropertyForm
 import form.WebsiteForPropertyForm.websiteForPropertyForm
-import views.html.login
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
