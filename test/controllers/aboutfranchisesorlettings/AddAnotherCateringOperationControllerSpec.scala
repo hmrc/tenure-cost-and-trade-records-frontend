78873d09c61e9b20a2ba366b1055107bc7ec99d8
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2023-06-15 15:19:33 +0100

    BST-55216 Update tests

--- test/controllers/aboutfranchisesorlettings/AddAnotherCateringOperationControllerSpec.scala
+++ test/controllers/aboutfranchisesorlettings/AddAnotherCateringOperationControllerSpec.scala
@@ -16,40 +16,66 @@
 
 package controllers.aboutfranchisesorlettings
 
-import navigation.AboutFranchisesOrLettingsNavigator
+import form.Errors
+import form.aboutfranchisesorlettings.AddAnotherCateringOperationOrLettingAccommodationForm.addAnotherCateringOperationForm
+import models.submissions.aboutfranchisesorlettings.AboutFranchisesOrLettings
 import play.api.http.Status
 import play.api.test.Helpers._
-import play.twirl.api.HtmlFormat
+import utils.FormBindingTestAssertions.mustContainError
 import utils.TestBaseSpec
-import views.html.aboutfranchisesorlettings.addAnotherCateringOperationOrLettingAccommodation
 
 class AddAnotherCateringOperationControllerSpec extends TestBaseSpec {
 
-  val mockAboutFranchisesOrLettingsNavigator = mock[AboutFranchisesOrLettingsNavigator]
+  import TestData._
 
-  val mockAddAnotherCateringOperationOrLettingAccommodation = mock[addAnotherCateringOperationOrLettingAccommodation]
-  when(mockAddAnotherCateringOperationOrLettingAccommodation.apply(any, any, any, any, any)(any, any))
-    .thenReturn(HtmlFormat.empty)
-
-  val addAnotherCateringOperationOrLettingAccommodationController =
+  def addAnotherCateringOperationOrLettingAccommodationController(
+    aboutFranchisesOrLettings: Option[AboutFranchisesOrLettings] = Some(prefilledAboutFranchiseOrLettings)
+  ) =
     new AddAnotherCateringOperationController(
       stubMessagesControllerComponents(),
-      mockAboutFranchisesOrLettingsNavigator,
-      mockAddAnotherCateringOperationOrLettingAccommodation,
-      preFilledSession,
+      aboutFranchisesOrLettingsNavigator,
+      addAnotherOperationConcessionFranchise,
+      preEnrichedActionRefiner(aboutFranchisesOrLettings = aboutFranchisesOrLettings),
       mockSessionRepo
     )
 
   "GET /" should {
     "return 200" in {
-      val result = addAnotherCateringOperationOrLettingAccommodationController.show(1)(fakeRequest)
+      val result = addAnotherCateringOperationOrLettingAccommodationController().show(1)(fakeRequest)
       status(result) shouldBe Status.OK
     }
 
     "return HTML" in {
-      val result = addAnotherCateringOperationOrLettingAccommodationController.show(1)(fakeRequest)
+      val result = addAnotherCateringOperationOrLettingAccommodationController().show(1)(fakeRequest)
       contentType(result) shouldBe Some("text/html")
       charset(result)     shouldBe Some("utf-8")
     }
+
+    "SUBMIT /" should {
+      "throw a BAD_REQUEST if an empty form is submitted" in {
+        val result = addAnotherCateringOperationOrLettingAccommodationController().submit(1)(fakeRequest)
+        status(result) shouldBe BAD_REQUEST
+      }
+    }
+  }
+
+  "Catering operation form" should {
+    "error if addAnotherCateringOperationOrLettingAccommodation is missing" in {
+      val formData = baseFormData - errorKey.addAnotherCateringOperationOrLettingAccommodation
+      val form     = addAnotherCateringOperationForm.bind(formData)
+
+      mustContainError(errorKey.addAnotherCateringOperationOrLettingAccommodation, Errors.booleanMissing, form)
+    }
+  }
+
+  object TestData {
+    val errorKey: Object {
+      val addAnotherCateringOperationOrLettingAccommodation: String
+    } = new {
+      val addAnotherCateringOperationOrLettingAccommodation: String =
+        "addAnotherCateringOperationOrLettingAccommodations"
+    }
+
+    val baseFormData: Map[String, String] = Map("addAnotherCateringOperationOrLettingAccommodations" -> "yes")
   }
 }
