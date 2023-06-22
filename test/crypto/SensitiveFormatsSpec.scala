1b7571a04b374bc929148aefa6889f40261a0429
PaingKaungVOA <paing.kaung@digital.voa.gov.uk>
2023-06-22 11:31:54 +0100

    BST-57013 encrypt PII data in session database. Test improvement.

---
+++ test/crypto/SensitiveFormatsSpec.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package crypto
+
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.matchers.should.Matchers
+import play.api.{Configuration, Environment, Mode}
+import uk.gov.hmrc.crypto.Sensitive.SensitiveString
+import play.api.libs.json.{Json, OFormat, Writes}
+import SensitiveFormats._
+
+import java.io.File
+
+class SensitiveFormatsSpec extends AnyWordSpecLike with Matchers {
+  import SensitiveFormatsSpec._
+
+  class TestMongoCrypto(configuration: Configuration) extends MongoCrypto(configuration) {
+    override protected val encryptionKey: String = configuration.get[String]("crypto.key")
+  }
+
+  val testEnv: Environment = Environment.simple(mode = Mode.Test)
+
+  val devSettings: Map[String, AnyRef] = Map(
+    "crypto.key" -> "P5xsJ9Nt+quxGZzB4DeLfw=="
+  )
+  val testConfig: Configuration = Configuration.load(testEnv, devSettings)
+
+  implicit val crypto: MongoCrypto = new TestMongoCrypto(testConfig)
+
+  "SensitiveFormats" should {
+
+    "serialize and deserialize SensitiveString correctly" in {
+      val originalString = "mySensitiveData"
+      val sensitiveString = SensitiveString(originalString)
+
+      val json = Json.toJson(sensitiveString)
+      val deserialized = Json.fromJson[SensitiveString](json)
+
+      deserialized.get shouldBe sensitiveString
+      deserialized.get.decryptedValue shouldBe originalString
+    }
+
+    "serialize and deserialize SensitiveTestEntity correctly" in {
+      val originalString = "mySensitiveData"
+      val sensitiveString = SensitiveString(originalString)
+      val testEntity = SensitiveTestEntity("normalString", sensitiveString)
+
+      val json = Json.toJson(testEntity)
+      val deserialized = Json.fromJson[SensitiveTestEntity](json)
+
+      deserialized.get shouldBe testEntity
+      deserialized.get.encryptedString shouldBe sensitiveString
+    }
+
+  }
+}
+
+object SensitiveFormatsSpec {
+
+  case class SensitiveTestEntity(
+                                  normalString    : String,
+                                  encryptedString : SensitiveString
+                                )
+
+  object SensitiveTestEntity {
+    import crypto.SensitiveFormats._
+    implicit def format(implicit crypto: MongoCrypto): OFormat[SensitiveTestEntity] = Json.format[SensitiveTestEntity]
+  }
+
+}
