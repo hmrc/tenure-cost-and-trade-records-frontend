3a94204243193713fc4ae6310a99136cd4e77d88
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2024-02-27 10:17:07 +0000

    BST-84285 - Ran precommit

--- app/controllers/aboutfranchisesorlettings/LettingOtherPartOfPropertyDetailsRentController.scala
+++ app/controllers/aboutfranchisesorlettings/LettingOtherPartOfPropertyDetailsRentController.scala
@@ -88,7 +88,7 @@ class LettingOtherPartOfPropertyDetailsRentController @Inject() (
       }
     }
   }
-  def submit(index: Int) = (Action andThen withSessionRefiner).async { implicit request =>
+  def submit(index: Int)                   = (Action andThen withSessionRefiner).async { implicit request =>
     val existingSection = request.sessionData.aboutFranchisesOrLettings.map(_.lettingSections).get(index)
     val forType         = request.sessionData.forType
 
