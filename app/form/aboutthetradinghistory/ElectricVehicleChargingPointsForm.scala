8ec881d876916fe96cd8b6c8625f70962a03c1ce
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2024-03-11 09:32:15 +0000

    BST-85948 - Removed unused imports

--- app/form/aboutthetradinghistory/ElectricVehicleChargingPointsForm.scala
+++ app/form/aboutthetradinghistory/ElectricVehicleChargingPointsForm.scala
@@ -16,15 +16,12 @@
 
 package form.aboutthetradinghistory
 
-import controllers.toOpt
 import form.ElectricVehicleChargingPointsMapping.validateSpacesOrBays
 import form.MappingSupport.createYesNoType
 import models.submissions.aboutthetradinghistory.ElectricVehicleChargingPoints
 import models.submissions.common.AnswerYes
 import play.api.data.Form
-import play.api.data.Forms.{default, mapping, optional, text}
-import play.api.data.validation.Constraints.{maxLength, nonEmpty}
-import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
+import play.api.data.Forms.mapping
 import uk.gov.voa.play.form.ConditionalMappings.mandatoryIfEqual
 
 import scala.util.Try
