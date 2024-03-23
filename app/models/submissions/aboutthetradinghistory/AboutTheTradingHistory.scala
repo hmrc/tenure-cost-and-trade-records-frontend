f92d2b4ed6f988a5b3313ff28dc459c95cedc0c3
Yuriy Tumakha <tumakha@gmail.com>
2024-03-23 22:17:15 +0000

    BST-86189 New TradingHistory property `turnoverSections6020: Option[Seq[TurnoverSection6020]]` and new model `TurnoverSection6020`

--- app/models/submissions/aboutthetradinghistory/AboutTheTradingHistory.scala
+++ app/models/submissions/aboutthetradinghistory/AboutTheTradingHistory.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import play.api.libs.json._
 case class AboutTheTradingHistory(
   occupationAndAccountingInformation: Option[OccupationalAndAccountingInformation] = None,
   turnoverSections: Seq[TurnoverSection] = Seq.empty,
+  turnoverSections6020: Option[Seq[TurnoverSection6020]] = None,
   turnoverSections6030: Seq[TurnoverSection6030] = Seq.empty,
   costOfSales: Seq[CostOfSales] = Seq.empty,
   fixedOperatingExpensesSections: Seq[FixedOperatingExpenses] = Seq.empty,
@@ -44,6 +45,7 @@ object AboutTheTradingHistory {
   implicit val aboutTheTradingHistoryReads: Reads[AboutTheTradingHistory] = (
     (__ \ "occupationAndAccountingInformation").readNullable[OccupationalAndAccountingInformation] and
       (__ \ "turnoverSections").read[Seq[TurnoverSection]] and
+      (__ \ "turnoverSections6020").readNullable[Seq[TurnoverSection6020]] and
       (__ \ "turnoverSections6030").readNullable[Seq[TurnoverSection6030]].map(_.getOrElse(Seq.empty)) and
       (__ \ "costOfSales").read[Seq[CostOfSales]] and
       (__ \ "fixedOperatingExpensesSections").read[Seq[FixedOperatingExpenses]] and
