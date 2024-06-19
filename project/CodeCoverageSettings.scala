4e36f5b2f3f091f33ee0e5af17dbe1c50042270e
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2024-06-19 15:48:01 +0100

    BST-93028 Increase coverage after merge

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -24,7 +24,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 85.6,
+    ScoverageKeys.coverageMinimumStmtTotal := 85.7,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
