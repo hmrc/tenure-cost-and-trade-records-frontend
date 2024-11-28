e47a9374dfc5a00e675b346981a443eaea00bd4c
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2024-11-28 15:15:16 +0000

    BST-112385 - Fixed navigation for Lease and Tenure for 6048

---
+++ test/navigation/AboutYourLeaseOrTenure6048NavigatorSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import connectors.Audit
+import models.ForType.*
+import models.submissions.aboutYourLeaseOrTenure.*
+import models.Session
+import models.submissions.common.AnswerNo
+import navigation.identifiers.*
+import play.api.libs.json.JsObject
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.TestBaseSpec
+
+import scala.concurrent.ExecutionContext
+
+class AboutYourLeaseOrTenure6048NavigatorSpec extends TestBaseSpec {
+
+  val audit = mock[Audit]
+  doNothing.when(audit).sendExplicitAudit(any[String], any[JsObject])(any[HeaderCarrier], any[ExecutionContext])
+
+  val navigator = new AboutYourLeaseOrTenureNavigator(audit)
+
+  val session6048 = Session(
+    "99996048004",
+    FOR6048,
+    prefilledAddress,
+    "Basic OTk5OTYwMTAwMDQ6U2Vuc2l0aXZlKC4uLik=",
+    isWelsh = false,
+    aboutLeaseOrAgreementPartOne = Some(prefilledAboutLeaseOrAgreement6045TextArea),
+    aboutLeaseOrAgreementPartThree = Some(prefilledAboutLeaseOrAgreementPartThree6045TextArea)
+  )
+
+  implicit override val hc: HeaderCarrier = HeaderCarrier()
+
+  "About your lease or tenure navigator" when {
+
+    "go to sign in from an identifier that doesn't exist in the route map" in {
+      case object UnknownIdentifier extends Identifier
+      navigator
+        .nextPage(UnknownIdentifier, session6048)
+        .apply(session6048) shouldBe controllers.routes.LoginController.show
+    }
+
+    "return a function that goes to how rent is currently fixed  page when UR building insurance is completed" in {
+      val answers = session6048.copy(
+        aboutLeaseOrAgreementPartTwo = Some(prefilledAboutLeaseOrAgreementPartTwo)
+      )
+      navigator
+        .nextPage(HowIsCurrentRentFixedId, answers)
+        .apply(answers) shouldBe controllers.aboutYourLeaseOrTenure.routes.MethodToFixCurrentRentController
+        .show()
+    }
+
+  }
+}
