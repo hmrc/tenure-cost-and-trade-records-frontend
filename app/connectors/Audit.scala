3f559af5e95eed95a257e9cc2a9dfd0f5e75e916
Pete Robinson <77154273+peterdrobinson@users.noreply.github.com>
2025-01-30 12:12:22 +0000

    BST-107041 - Removed CYA from contains and replaced with #, as CYA was providing false positives

--- app/connectors/Audit.scala
+++ app/connectors/Audit.scala
@@ -54,7 +54,7 @@ trait Audit extends AuditConnector {
     sendExplicitAudit("SavedAsDraft", savedAsDraftEvent)
 
   def sendChangeLink(pageID: String)(implicit request: SessionRequest[AnyContent], hc: HeaderCarrier): Unit =
-    if (request.getQueryString("from").contains("CYA")) {
+    if (request.getQueryString("from").contains("#")) {
       sendExplicitAudit("CyaChangeLink", ChangeLinkAudit(request.sessionData.forType.toString, request.uri, pageID))
     }
 
