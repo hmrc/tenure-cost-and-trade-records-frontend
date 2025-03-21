1060116f82a2fc76c2b15c73888b768dcc3419dc
MJANOWICZ <michal.janowicz@digital.voa.gov.uk>
2025-03-21 11:17:46 +0000

    BST-122201 Address lookup tests uncommented

--- test/connectors/AddressLookupConnectorSpec.scala
+++ test/connectors/AddressLookupConnectorSpec.scala
@@ -26,115 +26,115 @@ import play.api.libs.json.{JsValue, Json}
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 import play.api.mvc.Call
-//class AddressLookupConnectorSpec extends TestBaseSpec with WireMockHelper {
-//
-//  override def fakeApplication(): Application =
-//    new GuiceApplicationBuilder()
-//      .configure(
-//        "metrics.jvm"                                        -> false,
-//        "metrics.enabled"                                    -> false,
-//        "create-internal-auth-token-on-start"                -> false,
-//        "urls.tctrFrontend"                                  -> "someUrl",
-//        "microservice.services.address-lookup-frontend.port" -> 11111
-//      )
-//      .build()
-//
-//  private lazy val connector: AddressLookupConnector = inject[AddressLookupConnector]
-//  private implicit val language: Lang                = Lang("en")
-//  val call: Call                                     = Call("GET", "/callback-url")
-//  val testResponseAddress: JsValue                   =
-//    Json.parse(input =
-//      "{\n\"auditRef\":\"e9e2fb3f-268f-4c4c-b928-3dc0b17259f2\",\n\"address\":{\n\"lines\":[\n\"Line1\",\n\"Line2\",\n\"Line3\",\n\"Line4\"\n],\n \"postcode\":\"NE1 1LX\",\n\"country\":{\n\"code\":\"GB\",\n\"name\":\"United Kingdom\"\n}\n}\n}"
-//    )
-//
-//  "AddressLookupConnector" should {
-//
-//    "return a location when addressLookup.initialise" in {
-//      server.stubFor(
-//        post(urlEqualTo("/api/v2/init"))
-//          .willReturn(
-//            aResponse()
-//              .withStatus(202)
-//              .withHeader("Location", "/api/v2/location")
-//          )
-//      )
-//
-//      val result: Option[String] = Await.result(connector.initialise(call), 500.millisecond)
-//      result shouldBe Some("/api/v2/location")
-//    }
-//
-//    "return error when there is no Location" in {
-//      server.stubFor(
-//        post(urlEqualTo("/api/v2/init"))
-//          .willReturn(
-//            aResponse()
-//              .withStatus(202)
-//              .withBody("")
-//              .withHeader("notLocation", "")
-//          )
-//      )
-//
-//      val result: Option[String] = Await.result(connector.initialise(call), 500.millisecond)
-//      result shouldBe Some(
-//        s"[AddressLookupConnector][initialise] - Failed to obtain location from http://localhost:${server.port}/api/v2/init"
-//      )
-//    }
-//
-//    "return error when status is other than 202" in {
-//      server.stubFor(
-//        post(urlEqualTo("/api/v2/init"))
-//          .willReturn(
-//            aResponse()
-//              .withStatus(204)
-//          )
-//      )
-//
-//      val result: Option[String] = Await.result(connector.initialise(call), 500.millisecond)
-//      result shouldBe None
-//    }
-//
-//    "get None when there is an error" in {
-//      server.stubFor(
-//        post(urlEqualTo("/api/v2/init"))
-//          .willReturn(
-//            aResponse()
-//          )
-//      )
-//
-//      val result: Future[Option[String]] = connector.initialise(call)
-//      whenReady(result) { res =>
-//        res shouldBe None
-//      }
-//
-//    }
-//
-//    "return None when HTTP call fails" in {
-//      server.stubFor(
-//        post(urlEqualTo("/api/v2/init"))
-//          .willReturn(
-//            aResponse()
-//              .withStatus(400)
-//          )
-//      )
-//
-//      val result: Future[Option[String]] = connector.initialise(call)
-//      whenReady(result) { res =>
-//        res shouldBe None
-//      }
-//    }
-//
-//    "return cacheMap when called with ID" in {
-//      server.stubFor(
-//        get(urlEqualTo("/api/confirmed?id=123456789"))
-//          .willReturn(
-//            aResponse()
-//              .withStatus(200)
-//              .withBody(testResponseAddress.toString)
-//          )
-//      )
-//
-//      val result = Await.result(connector.getAddress(id = "123456789"), 5.second)
-//      result.auditRef.map(_ shouldBe "e9e2fb3f-268f-4c4c-b928-3dc0b17259f2")
-//    }
-//  }
-//}
+class AddressLookupConnectorSpec extends TestBaseSpec with WireMockHelper {
+
+  override def fakeApplication(): Application =
+    new GuiceApplicationBuilder()
+      .configure(
+        "metrics.jvm"                                        -> false,
+        "metrics.enabled"                                    -> false,
+        "create-internal-auth-token-on-start"                -> false,
+        "urls.tctrFrontend"                                  -> "someUrl",
+        "microservice.services.address-lookup-frontend.port" -> 11111
+      )
+      .build()
+
+  private lazy val connector: AddressLookupConnector = inject[AddressLookupConnector]
+  private implicit val language: Lang                = Lang("en")
+  val call: Call                                     = Call("GET", "/callback-url")
+  val testResponseAddress: JsValue                   =
+    Json.parse(input =
+      "{\n\"auditRef\":\"e9e2fb3f-268f-4c4c-b928-3dc0b17259f2\",\n\"address\":{\n\"lines\":[\n\"Line1\",\n\"Line2\",\n\"Line3\",\n\"Line4\"\n],\n \"postcode\":\"NE1 1LX\",\n\"country\":{\n\"code\":\"GB\",\n\"name\":\"United Kingdom\"\n}\n}\n}"
+    )
+
+  "AddressLookupConnector" should {
+
+    "return a location when addressLookup.initialise" in {
+      server.stubFor(
+        post(urlEqualTo("/api/v2/init"))
+          .willReturn(
+            aResponse()
+              .withStatus(202)
+              .withHeader("Location", "/api/v2/location")
+          )
+      )
+
+      val result: Option[String] = Await.result(connector.initialise(call), 500.millisecond)
+      result shouldBe Some("/api/v2/location")
+    }
+
+    "return error when there is no Location" in {
+      server.stubFor(
+        post(urlEqualTo("/api/v2/init"))
+          .willReturn(
+            aResponse()
+              .withStatus(202)
+              .withBody("")
+              .withHeader("notLocation", "")
+          )
+      )
+
+      val result: Option[String] = Await.result(connector.initialise(call), 500.millisecond)
+      result shouldBe Some(
+        s"[AddressLookupConnector][initialise] - Failed to obtain location from http://localhost:${server.port}/api/v2/init"
+      )
+    }
+
+    "return error when status is other than 202" in {
+      server.stubFor(
+        post(urlEqualTo("/api/v2/init"))
+          .willReturn(
+            aResponse()
+              .withStatus(204)
+          )
+      )
+
+      val result: Option[String] = Await.result(connector.initialise(call), 500.millisecond)
+      result shouldBe None
+    }
+
+    "get None when there is an error" in {
+      server.stubFor(
+        post(urlEqualTo("/api/v2/init"))
+          .willReturn(
+            aResponse()
+          )
+      )
+
+      val result: Future[Option[String]] = connector.initialise(call)
+      whenReady(result) { res =>
+        res shouldBe None
+      }
+
+    }
+
+    "return None when HTTP call fails" in {
+      server.stubFor(
+        post(urlEqualTo("/api/v2/init"))
+          .willReturn(
+            aResponse()
+              .withStatus(400)
+          )
+      )
+
+      val result: Future[Option[String]] = connector.initialise(call)
+      whenReady(result) { res =>
+        res shouldBe None
+      }
+    }
+
+    "return cacheMap when called with ID" in {
+      server.stubFor(
+        get(urlEqualTo("/api/confirmed?id=123456789"))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withBody(testResponseAddress.toString)
+          )
+      )
+
+      val result = Await.result(connector.getAddress(id = "123456789"), 5.second)
+      result.auditRef.map(_ shouldBe "e9e2fb3f-268f-4c4c-b928-3dc0b17259f2")
+    }
+  }
+}
