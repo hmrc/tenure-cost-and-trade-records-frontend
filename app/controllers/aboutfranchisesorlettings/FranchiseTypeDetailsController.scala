73d6b990794960eec44e2b660410b5c28cc59587
Yuriy Tumakha <tumakha@gmail.com>
2025-04-22 16:22:36 +0100

    BST-125028 Increase test coverage from 83.7% to 84.3%

--- app/controllers/aboutfranchisesorlettings/FranchiseTypeDetailsController.scala
+++ app/controllers/aboutfranchisesorlettings/FranchiseTypeDetailsController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,7 +89,6 @@ class FranchiseTypeDetailsController @Inject() (
           )
         ),
       formData =>
-        given Session = request.sessionData
         for
           (newSession, updatedIndex) <- successful(sessionWithBusinessDetails(formData, idx))
           _                          <- repository.saveOrUpdate(newSession)
