a57488717d883068cfc89c60b3fd9ae7f70e0813
Yuriy Tumakha <tumakha@gmail.com>
2025-05-30 10:41:23 +0100

    BST-128449 6020, 6030, 6076 - Insert warning page where no financial date changes made

--- test/views/aboutYourLeaseOrTenure/RentIncludeTradeServicesDetailsTextAreaViewSpec.scala
+++ test/views/aboutYourLeaseOrTenure/RentIncludeTradeServicesDetailsTextAreaViewSpec.scala
@@ -16,6 +16,7 @@
 
 package views.aboutYourLeaseOrTenure
 
+import actions.SessionRequest
 import form.aboutYourLeaseOrTenure.RentIncludeTradeServicesDetailsTextAreaForm
 import models.ForType.*
 import models.pages.Summary
@@ -29,10 +30,12 @@ class RentIncludeTradeServicesDetailsTextAreaViewSpec extends QuestionViewBehavi
   override val form =
     RentIncludeTradeServicesDetailsTextAreaForm.rentIncludeTradeServicesDetailsTextAreaForm
 
-  def createView = () => rentIncludeTradeServicesDetailsTextAreaView(form)(using fakeRequest, messages)
+  val sessionRequest = SessionRequest(aboutYourTradingHistory6076YesSession, fakeRequest)
+
+  def createView = () => rentIncludeTradeServicesDetailsTextAreaView(form)(using sessionRequest, messages)
 
   def createViewUsingForm = (form: Form[String]) =>
-    rentIncludeTradeServicesDetailsTextAreaView(form)(using fakeRequest, messages)
+    rentIncludeTradeServicesDetailsTextAreaView(form)(using sessionRequest, messages)
 
   "Rent include trade services details" must {
 
