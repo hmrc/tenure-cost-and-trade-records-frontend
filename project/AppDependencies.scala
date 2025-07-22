739c2980c1d0a2751ac9e49a596cc13c8eb9da6c
Yuriy Tumakha <tumakha@gmail.com>
2025-07-22 17:16:51 +0100

    BST-134708 (6015/6016) - Updated in accordance with latest design in Mural

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion              = "9.16.0"
+  val bootstrapVersion              = "9.18.0"
   val playFrontendVersion           = "12.7.0"
   val playConditionalMappingVersion = "3.3.0"
   val mongoVersion                  = "2.7.0"
